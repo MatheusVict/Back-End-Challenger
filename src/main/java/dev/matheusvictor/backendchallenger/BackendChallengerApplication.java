f9a747f34e2fc47f0a6b042dbb28c58612e42a43
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-14 15:12:57 -0300

    inital commit

---
+++ src/main/java/dev/matheusvictor/backendchallenger/BackendChallengerApplication.java
@@ -0,0 +1,13 @@
+package dev.matheusvictor.backendchallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengerApplication.class, args);
+	}
+
+}
